88c22777d9e44e4dfc1453e910d34a28fd4c6371
Chryus <chris.haack@me.com>
2013-11-14 19:49:12 -0500

    add until loop to number guesser

--- Number-Guesser/number-guesser.rb
+++ Number-Guesser/number-guesser.rb
@@ -1,9 +1,10 @@
 
-print "Heya. Please enter a number between 1 & 100: "
-number_guess = gets.chomp.to_i
-number = rand(100) + 1
-
-puts number_guess == number
+#until loop runs until number_guess equals the random number
+  begin
+  	print "Heya. Please enter a number between 1 & 100: "
+	number_guess = gets.chomp.to_i
+	number = rand(100) + 1
+  end until number_guess == number
 
 
 
